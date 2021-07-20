9faf43d1a122b1ef2ccc9bea9ac85b34d5fedfb3
Kerim Hudson <code@kerim.sh>
2021-07-20 09:47:50 +0200

    Build out input component; add headless ui; add tailwind styles to app

---
+++ pages/components.js
@@ -0,0 +1,11 @@
+import Input from "~/components/Input"
+/**
+ * You can utilise this page to test out your various components!
+ */
+const ComponentsPage = () => {
+    return <div className="p-12">
+        <Input />
+    </div>
+}
+
+export default ComponentsPage
