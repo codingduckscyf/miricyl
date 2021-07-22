64c07a5ea8da9752e4058aea83fd9b6bd55a71d3
Oksana Kostenko <kseniavlad31@gmail.com>
2021-07-22 13:37:44 +0100

    add CalloutBox component

---
+++ components/CalloutBox/CalloutBoxButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CalloutBoxButton = ({ buttonClickHandler, buttonText }) => {
+  return (
+    <button
+      onClick={buttonClickHandler}
+      type="button"
+      className="inline-flex items-center mt-8 px-2.5 py-3  border border-transparent text-xs font-medium rounded shadow-sm text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+    >
+      {buttonText}
+    </button>
+  );
+};
+
+export default CalloutBoxButton;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Footer from "~/components/Footer";
+import CalloutBox from "~/components/CalloutBox";
+
+// Button Link / onClick function
+// Button Text
+// Caption /
+// Heading Text /
+// Image Url /
+// Image Alt Tag /
 
 const Index = () => {
   return (
     <div>
       <h2>Welcome to NextJS!</h2>
+      <CalloutBox
+        heading="Mental health support for everyone"
+        caption="Miricly’s knowledge base is a curated collection of wonderful content designed to support and aid everyone in their journey to better mental health."
+        imgUrl="https://ichef.bbci.co.uk/childrens-responsive-ichef-ck/400xn/amz/cbbc/bp-duckling.jpg"
+        imgAltText="duckling"
+        buttonText="Learn more about Miricyl's vision"
+        buttonClickHandler={() =>
+          console.log(`Learn more about Miricyl's vision was clicked!`)
+        }
+      />
       <Footer />
     </div>
   );
