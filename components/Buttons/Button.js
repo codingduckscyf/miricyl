a89b7dbaf06b18e8a04bc7e04cfdc1ef8edf870a
NeginDn <negin.derakhshaan@gmail.com>
2021-07-22 12:51:21 +0100

    Onclick added

--- components/Buttons/Button.js
+++ components/Buttons/Button.js
@@ -1,7 +1,7 @@
-const Button = ({ caption }) => {
+const Button = ({ caption, onClick }) => {
   return (
     <div className="items-center">
-      <button
+      <button onClick={onClick}
         type="button"
         className=" inline-flex items-center px-8 py-4 border border-transparent text-xl font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-10"
       >
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,6 +1,31 @@
 import Image from "next/image";
 import Button from "../Buttons/Button";
 
+// {
+//   listItems.map((item, index) => (
+//     <li key={index} className="text-gray-500 text-sm">
+//       <Link href="/">
+//         <a>{item}</a>
+//       </Link>
+//     </li>
+//   ));
+// }
+
+const dummyCategories = [
+  "Depression",
+  "Anxiety",
+  "Addictions",
+  "Anger",
+  "Eating problems",
+];
+
+const handleClickCategories = () => {
+    dummyCategories.map(category => { 
+        console.log(category)
+    })
+};
+
+
 const Header = ({ pageHeader }) => {
   const { backgroundColour, imgSrc, imgAlt, title, caption } = pageHeader;
   return (
@@ -9,7 +34,7 @@ const Header = ({ pageHeader }) => {
         <div className="self-center mx-16 w-1/2">
           <h2 className="py-7 font-bold text-8xl">{title}</h2>
           <p className=" text-2xl text-gray-500">{caption}</p>
-          <Button caption={"View Categories"} />
+          <Button caption={"View Categories"} onClick={handleClickCategories} />
         </div>
         <div className="md:flex-shrink-0 h-full">
           <Image className=" rounded-l-3xl" src={imgSrc} alt={imgAlt} />
