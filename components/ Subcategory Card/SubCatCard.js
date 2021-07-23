0c9c90db9bacaf92216f64e9d8a7533a38b6c3c3
NeginDn <negin.derakhshaan@gmail.com>
2021-07-23 09:30:17 +0100

    Tyding up the code based on reviews

--- components/ Subcategory Card/SubCatCard.js
+++ components/ Subcategory Card/SubCatCard.js
@@ -1,22 +1,24 @@
 import Link from "next/link";
-const SubCatCard = ({ backgroundColour, title, category, link }) => {
-  console.log(backgroundColour);
+const SubCatCard = ({ backgroundColor, title, category, link }) => {
   return (
     <div>
       <Link href={link}>
-        <div
-          className={
-            "w-64 md:w-96 h-24 md:h-48 m bg-" +
-            backgroundColour +
-            "-300 rounded-2xl flex flex-col justify-end text-white"
-          }
-        >
-          <h3 className="  text-base md:text-2xl block text-white">
-            {category}
-          </h3>
-          <h4 className="text-2xl md:text-4xl font-bold text-white pb-10">{title}</h4>
-          <a></a>
-        </div>
+        <a>
+          <div
+            className={
+              "w-64 md:w-96 h-24 md:h-48 m bg-" +
+              backgroundColor +
+              "-300 rounded-2xl flex flex-col justify-end text-white"
+            }
+          >
+            <h3 className="  text-base md:text-2xl block text-white">
+              {category}
+            </h3>
+            <h4 className="text-2xl md:text-4xl font-bold text-white pb-10">
+              {title}
+            </h4>
+          </div>
+        </a>
       </Link>
     </div>
   );
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,30 +1,24 @@
 import Image from "next/image";
-import SubCatCard from "../ Subcategory Card/SubCatCard";
 import Button from "../Buttons/Button";
 
-
-const dummyCategories = [
-  "Depression",
-  "Anxiety",
-  "Addictions",
-  "Anger",
-  "Eating problems",
-];
-
-const handleClickCategories = () => {
-    dummyCategories.map(category => { 
-        console.log(category)
-    })
+const handleClick = () => {
+  console.log("Hello from buttons world");
 };
 
-const Header = ({ backgroundColour, imgSrc, imgAlt, title, caption }) => {
+const Header = ({ backgroundColor, imgSrc, imgAlt, title, caption }) => {
   return (
-      <header style={{ backgroundColor: { backgroundColour } }}>
+    <header
+      className={
+        "bg-" +
+        backgroundColor +
+        "-100 "
+      }
+    >
       <div className="flex justify-between">
         <div className="self-center mx-16 w-1/2">
           <h2 className="py-7 font-bold text-4xl md:text-8xl">{title}</h2>
           <p className="text-xl md:text-2xl text-gray-500">{caption}</p>
-          <Button caption={"View Categories"} onClick={handleClickCategories} />
+          <Button caption={"View Categories"} onClick={handleClick} />
         </div>
         <div className="md:flex-shrink-0 h-full">
           <Image className="rounded-l-3xl" src={imgSrc} alt={imgAlt} />
