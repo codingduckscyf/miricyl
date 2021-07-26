ffe4c7254c756931b9d4f13f45c8c8b2985012fb
Oksana Kostenko <kseniavlad31@gmail.com>
2021-07-26 15:18:07 +0100

    removes SiteHeader and Footer from Index.js page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
-import Footer from "~/components/Footer";
 import CalloutBox from "~/components/CalloutBox";
 import Header from "~/components/Header";
 import headerImg from "../public/images/headerImg.jpeg";
-import SiteHeader from "~/components/SiteHeader";
 import calloutImg from "../public/images/callOutBox.jpeg";
 import CategoriesSelection from "~/components/CategorySelection";
 
 const Index = () => {
   return (
     <div>
-      <SiteHeader />
       <Header
         backgroundColor="white"
         imgSrc={headerImg}
@@ -19,7 +16,7 @@ const Index = () => {
           "Miricyl helps you find key, informative resources to guide you on your mental health journey"
         }
       />
-      <CategoriesSelection/>
+      <CategoriesSelection />
       <CalloutBox
         heading="Mental health support for everyone"
         caption="Miricly’s knowledge base is a curated collection of wonderful content designed to support and aid everyone in their journey to better mental health."
@@ -28,7 +25,6 @@ const Index = () => {
         buttonText="Learn more about Miricyl's vision"
         buttonLink="/mental-health"
       />
-      <Footer />
     </div>
   );
 };
