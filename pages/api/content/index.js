62f11233f8a127f122e08ddca8925026b8a8841e
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-07-26 14:52:42 +0100

    added suggested improvements

--- pages/api/content/index.js
+++ pages/api/content/index.js
@@ -2,7 +2,7 @@ import sql from "~/lib/postgres";
 
 const handler = async (req, res) => {
 	if (req.method === "GET") {
-		res.status(200).json({ data: await sql`SELECT * FROM content;` });
+		return res.status(200).json({ data: await sql`SELECT * FROM content;` });
 	}
 	if (req.method === "POST") {
 		const { title, content_type, description, issueID, img_url, video_url } = req.body;
@@ -10,20 +10,21 @@ const handler = async (req, res) => {
 		const checkContentTitle = await sql`SELECT * FROM content WHERE title=${title};`;
 
 		const checkIssueID = await sql`SELECT * FROM issues WHERE id=${issueID};`;
-		console.log(checkIssueID)
 
 		if (!title || !content_type || !description || !issueID) {
-			res.status(400).json({ message: "Please fill the required fields" });
+			return res.status(400).json({ message: "Please fill the required fields" });
 		}
 		if (checkContentTitle.count !== 0) {
-			res.status(400).json({ message: `Content with the title: ${title} already exists` })
+			return res.status(400).json({ message: `Content with the title: ${title} already exists` })
 		}
 		if (checkIssueID.count === 0) {
-			res.status(400).json({ message: `There is no issue with the id: ${issueID}` })
+			return res.status(400).json({ message: `There is no issue with the id: ${issueID}` })
 		} else {
-			const insertContentData = await sql`INSERT INTO content (title, description, content_type, video_url, img_url) VALUES (${title}, ${description}, ${content_type}, ${video_url}, ${img_url}) RETURNING id;`;
-			const insertContentAndIssueIDs = await sql`INSERT INTO issue_content_rel (issue_id, content_id) values (${Number(issueID)}, ${insertContentData[0].id}) RETURNING issue_id, content_id;`;
-			res.status(200).json({ message: `ContentID: ${insertContentAndIssueIDs[0].issue_id} and issueID: ${insertContentAndIssueIDs[0].content_id}` });
+			const [content] = await sql`INSERT INTO content (title, description, content_type, video_url, img_url) VALUES (${title}, ${description}, ${content_type}, ${video_url}, ${img_url}) RETURNING id;`;
+
+			const [relation] = await sql`INSERT INTO issue_content_rel (issue_id, content_id) values (${issueID}, ${content.id}) RETURNING issue_id, content_id;`;
+
+			return res.status(200).json({ message: `ContentID: ${relation.issue_id} and issueID: ${relation.content_id}` });
 		}
 	}
 }
