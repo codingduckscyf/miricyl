bdca32bb77e74ab803c0ca96f5a16c00e2e36bf2
Oksana Kostenko <kseniavlad31@gmail.com>
2021-07-26 15:19:12 +0100

    adds temp content fro issues and about pages

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,11 @@
 const About = () => {
-    return <div>About this page.</div>
-}
+  return (
+    <div>
+      <h1 className="text-green-800 bg-green-300 font-bold text-5xl p-32">
+        This is About page
+      </h1>
+    </div>
+  );
+};
 
 export default About;
--- pages/life-issues.js
+++ pages/life-issues.js
@@ -1,10 +1,8 @@
 const LifeIssues = () => {
   return (
-    <div>
-      <h1 className="text-blue-800 bg-red-300 font-bold text-5xl p-32">
-        Life issues page
-      </h1>
-    </div>
+    <h1 className="text-red-800 bg-yellow-300 font-bold text-5xl p-32">
+      Hey hey hey from life issues page!
+    </h1>
   );
 };
 
