d726c844cc68937795ce1287ad8b3951ede54d65
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-07-27 12:12:10 +0100

    Fixed some queries in the API GET endpoints

---
+++ pages/api/categories/[id]/index.js
@@ -0,0 +1,9 @@
+import sql from "~/lib/postgres";
+
+const handler = async ({ query: { id } }, res) => {
+	res
+		.status(200)
+		.json({ data: (await sql`SELECT * FROM categories WHERE id=${id};`)[0] });
+};
+
+export default handler;
--- pages/api/issues/[id]/content.js
+++ pages/api/issues/[id]/content.js
@@ -6,7 +6,7 @@ const handler = async ({ method, query: { id } }, res) => {
 		if (checkID.count === 0) {
 			res.status(400).json({ message: `There is no content related to this issue id: ${id}` })
 		} else {
-			res.status(200).json({ data: await sql`SELECT content.* FROM content INNER JOIN issue_content_rel on content.id= issue_content_rel.content_id INNER JOIN issues on issues.id=${id};` })
+			res.status(200).json({ data: await sql`SELECT content.* FROM content INNER JOIN issue_content_rel on content.id= issue_content_rel.content_id INNER JOIN issues on issue_content_rel.issue_id=issues.id WHERE issues.id=${id};` })
 		}
 	}
 }
