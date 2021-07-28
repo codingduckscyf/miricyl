9af4e082ff387d48454269a62357e1e36e6bee05
Kerim Hudson <code@kerim.sh>
2021-07-28 09:36:15 +0200

    fix map to work correctly

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -2,13 +2,12 @@ import useSWR from "swr";
 
 const Categories = () => {
   const { data } = useSWR("/api/categories");
-  const test = data && JSON.stringify(data, null, 2);
   return (
     <div>
       <h2>Categories to be shown here</h2>
-      {test.map(item, (index) => (
-        <li key={index}>{item}</li>
-      ))}
+      {data
+        ? data.data.map((item, index) => <li key={index}>{item.name}</li>)
+        : null}
     </div>
   );
 };
