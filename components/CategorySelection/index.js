1466d460d5edf743144ca438e11a9680bc1b9b16
NeginDn <negin.derakhshaan@gmail.com>
2021-07-28 15:18:56 +0100

    Getting category from api instead of an array

--- components/CategorySelection/index.js
+++ components/CategorySelection/index.js
@@ -1,26 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
+import useSWR from "swr";
 import React from "react";
 import Card from "./card";
 
 const CategoriesSelection = ({ url, title }) => {
-  //TODO: fetch data from API
-  const arrayData = [
-    {
-      id: "12",
-      title: "Anxiety",
-      imgUrl: "https://picsum.photos/200/?random1",
-    },
-    {
-      id: "12",
-      title: "Depression",
-      imgUrl: "https://picsum.photos/200/?random2",
-    },
-    {
-      id: "12",
-      title: "Paranoia",
-      imgUrl: "https://picsum.photos/200/?random3",
-    },
-  ];
+  const { data, error } = useSWR("/api/categories");
+
+  if (!data && !error) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data) {
+    return <div>Not found.</div>;
+  }
   return (
     <div className=" mt-16 md:mt-32 mb-4 mx-10 flex flex-col flex-grow items-center justify-center">
       <div>
@@ -30,13 +22,14 @@ const CategoriesSelection = ({ url, title }) => {
         </p>
       </div>
       <div className="container mx-auto">
-        <div className="flex flex-wrap items-center justify-center">
-          {arrayData.slice(0, 3).map((item) => (
+        <div className="flex flex-wrap items-center justify-center font-bold">
+          {data.data.map((item) => (
             <Card
               key={item.id}
               id={item.id}
-              title={item.title}
+              title={item.name}
               url={item.imgUrl}
+              link={`${item.name}`}
             />
           ))}
         </div>
