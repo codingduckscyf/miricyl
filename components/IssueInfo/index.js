e566fdbf7d3a390ec4541aa9e03f122aa86d0a88
Oksana Kostenko <kseniavlad31@gmail.com>
2021-08-01 08:41:32 +0100

    changes some stylings

--- components/IssueInfo/index.js
+++ components/IssueInfo/index.js
@@ -1,8 +1,10 @@
 const IssueInfo = ({ issueTitle, issueDescription }) => {
   return (
-    <div className="bg-yellow-100 p-10 sm:p-16">
-      <h1 className="text-4xl sm:text-6xl font-extrabold py-8">{issueTitle}</h1>
-      <p className="text-xl sm:text-2xl text-gray-600">{issueDescription}</p>
+    <div className="bg-gradient-to-r from-blue-100  to-blue-200 p-10 py-16 sm:px-20">
+      <h1 className="text-4xl sm:text-6xl font-bold py-8">{issueTitle}</h1>
+      <p className="text-l sm:text-xl text-gray-600 pr-64">
+        {issueDescription}
+      </p>
     </div>
   );
 };
