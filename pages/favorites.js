7a041a9c36098c138e69d771406d8da212746a77
Oksana Kostenko <kseniavlad31@gmail.com>
2021-08-02 18:00:15 +0100

    created fovorites page

---
+++ pages/favorites.js
@@ -0,0 +1,13 @@
+import Layout from "~/components/Layout/Layout";
+
+const Favorites = () => {
+  return (
+    <Layout>
+      <div className="bg-indigo-400 p-32">
+        <h1 className="text-4xl">My favorite page!</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Favorites;
