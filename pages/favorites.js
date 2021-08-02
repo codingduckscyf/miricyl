512132ce447c273fd9f701a60addd424b194c58e
Oksana Kostenko <kseniavlad31@gmail.com>
2021-08-02 18:17:21 +0100

    added titles

--- pages/favorites.js
+++ pages/favorites.js
@@ -3,8 +3,13 @@ import Layout from "~/components/Layout/Layout";
 const Favorites = () => {
   return (
     <Layout>
-      <div className="bg-indigo-400 p-32">
-        <h1 className="text-4xl">My favorite page!</h1>
+      <div className="bg-green-200 p-32">
+        <h1 className="text-4xl">Your favorite items</h1>
+        <div>
+          <h3 className="text-2xl">Articles</h3>
+          <h3 className="text-2xl">Videos</h3>
+          <h3 className="text-2xl">Useful Links</h3>
+        </div>
       </div>
     </Layout>
   );
