19c9ebb92e6c2c8fcd271ae8413eb701157ace34
NeginDn <negin.derakhshaan@gmail.com>
2021-08-03 12:29:25 +0100

    Add the heart icon to contents

--- pages/issues/[id].js
+++ pages/issues/[id].js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import { useRouter } from "next/dist/client/router";
 import ContentCard from "~/components/ContentCard";
 import IssueInfo from "~/components/IssueInfo";
+import useLocalStorage from "~/lib/useLocalStorage";
 
 const Issue = () => {
   const router = useRouter();
@@ -13,6 +14,19 @@ const Issue = () => {
   const { data: issueInfo, error: issueInfoError } = useSWR(
     `/api/issues/${id}`
   );
+
+  const [favorites, setFavorites] = useLocalStorage("favoriteContents", []);
+
+  const heartIconClickHandler = (id) => {
+    const newFavorites = [...favorites];
+    const index = newFavorites.indexOf(id);
+    if (index === -1) {
+      newFavorites.push(id);
+    } else {
+      newFavorites.splice(index, 1);
+    }
+    setFavorites(newFavorites);
+  };
 
   if (!issueInfo && issueInfoError) {
     return <div>Loading...</div>;
@@ -37,19 +51,21 @@ const Issue = () => {
         issueDescription={issueInfo.data.description}
       />
       <ul className="flex flex-col sm:flex-row flex-wrap justify-between p-4 my-12">
-        {issueContent.data.map((issue) => (
-          <ContentCard
-            key={issue.id}
-            imgSrc={issue.img_url ?? "https://picsum.photos/100"}
-            imgAlt={issue.title}
-            contentType={issue.content_type}
-            title={issue.title}
-            caption={issue.description}
-            link={
-              issue.video_url ?? "https://en.wikipedia.org/wiki/Mental_health"
-            }
-          />
-        ))}
+        {issueContent.data.map(
+          ({ id, img_url, title, content_type, description, video_url }) => (
+            <ContentCard
+              key={id}
+              isLiked={favorites.includes(id)}
+              imgSrc={img_url ?? "https://picsum.photos/100"}
+              imgAlt={title}
+              contentType={content_type}
+              title={title}
+              caption={description}
+              link={video_url ?? "https://en.wikipedia.org/wiki/Mental_health"}
+              heartIconClickHandler={() => heartIconClickHandler(id)}
+            />
+          )
+        )}
       </ul>
     </Layout>
   );
