e983d123152fe37780bbb07239329cd1e9077d30
Oksana Kostenko <kseniavlad31@gmail.com>
2021-08-04 16:51:57 +0100

    fixed an issue

--- components/Buttons/Button.js
+++ components/Buttons/Button.js
@@ -1,17 +1,18 @@
-const Button = ({ caption, onClick }) => {
+import Link from "next/link";
+
+const Button = ({ caption, link }) => {
   return (
-    <div className="items-center">
-      <button
-        onClick={onClick}
-        type="button"
-        className="inline-flex items-center mt-8 p-4 md:p-5 border border-transparent text-xs font-medium rounded-lg shadow-sm text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 text-base md:text-xl"
-      >
-        {caption}
-      </button>
-    </div>
+    <Link href={link} className="items-center">
+      <a>
+        <button
+          type="button"
+          className="inline-flex items-center mt-8 p-4 md:p-5 border border-transparent text-xs font-medium rounded-lg shadow-sm text-white bg-blue-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 text-base md:text-xl"
+        >
+          {caption}
+        </button>
+      </a>
+    </Link>
   );
 };
 
 export default Button;
-
- 
