93c24652a6e567e8eb6ef5c628e7d0a810db3506
Oksana Kostenko <kseniavlad31@gmail.com>
2021-08-06 20:00:51 +0100

    add-content changes

--- pages/add-content.js
+++ pages/add-content.js
@@ -1,8 +1,14 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import Form from "~/components/Form";
 import Layout from "~/components/Layout/Layout";
 
 const AddContentPage = () => {
+  const router = useRouter();
+  const [error, setError] = useState(false);
+
   const handleAddContentSubmit = async (data) => {
+    setError(false);
     const response = await fetch("/api/content", {
       method: "POST",
       headers: {
@@ -11,8 +17,12 @@ const AddContentPage = () => {
       },
       body: JSON.stringify(data),
     });
-    console.log("submit", data, "response", response);
-    return response;
+
+    if (response.ok) {
+      router.push(`/issues/${data.relations[0].id}`);
+    } else {
+      setError(true);
+    }
   };
 
   //data for form
@@ -22,11 +32,14 @@ const AddContentPage = () => {
     content_type: "",
     img_url: "",
     video_url: "",
+    relations: [],
   };
 
   return (
     <Layout>
       <div>
+        {error && <h1>Please check your form</h1>}
+
         <h1 className="bg-yellow-400 p-32 text-6xl">Well hello again</h1>
         <Form data={data} submit={handleAddContentSubmit} />
       </div>
