d675c19714f2192c8e035512740c38096daa5dc3
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-08-06 21:00:35 +0100

    relations

--- pages/api/content/index.js
+++ pages/api/content/index.js
@@ -1,13 +1,19 @@
 import sql from "~/lib/postgres";
 
 const handler = async (req, res) => {
-  const issuesTest = await sql`SELECT * FROM issues;`;
-  console.log(issuesTest);
   if (req.method === "GET") {
     return res.status(200).json({ data: await sql`SELECT * FROM content;` });
   }
   if (req.method === "POST") {
-    const { title, content_type, description, img_url, video_url } = req.body;
+    const { title, content_type, description, img_url, video_url, relations } =
+      req.body;
+    const contentObj = {
+      title: title,
+      content_type: content_type,
+      img_url: img_url ? img_url : null,
+      video_url: video_url ? video_url : null,
+      description: description,
+    };
 
     if (!title || !content_type || !description) {
       return res
@@ -22,11 +28,16 @@ const handler = async (req, res) => {
         .status(400)
         .json({ message: `Content with the title: ${title} already exists` });
     } else {
-      const [content] =
-        await sql`INSERT INTO content (title, description, content_type, video_url, img_url) VALUES (${title}, ${description}, ${content_type}, ${video_url}, ${img_url}) RETURNING *;`;
+      const [content] = await sql`INSERT INTO content ${sql(
+        contentObj,
+        ...Object.keys(contentObj)
+      )} RETURNING *;`;
+      const [issue_rel] =
+        await sql`INSERT INTO issue_content_rel (issue_id, content_id) VALUES (${relations}, ${content.id}) RETURNING *;`;
       return res.status(200).json({
         message: `Content with the id: ${content.id} was added to the database`,
         data: content,
+        relation: issue_rel,
       });
     }
   }
