6300d0b8bf539c1d5c0eae8b145dee00407dc077
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-08-09 19:16:25 +0100

    login page

--- components/SiteHeader/CategoriesList.js
+++ components/SiteHeader/CategoriesList.js
@@ -4,15 +4,15 @@ import useSWR from "swr";
 const CategoriesList = () => {
   const { data: categories } = useSWR("/api/categories");
   return (
-    <div>
-      {categories &&
-        categories.data.map(({ id, slug, name }) => (
-          <Link href={`/categories/${slug}`} key={id}>
-            <a className="block hover:bg-gray-200 p-6">
-              <h3 className="font-extrabold">{name}</h3>
-            </a>
-          </Link>
-        ))}
+    <div className="border rounded-md border-none overflow-hidden">
+      {categories.map((category) => (
+        <Link href={category.link} key={category.id}>
+          <a className="block hover:bg-gray-200 p-6">
+            <h3 className="font-extrabold">{category.title}</h3>
+            <p className="text-gray-500">{category.description}</p>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
