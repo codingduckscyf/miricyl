2001e15109bc7a8e5dc1dc73012b21dbfd6a2864
NeginDn <negin.derakhshaan@gmail.com>
2021-08-10 10:40:32 +0100

    Getting the drop menue from database

--- components/SiteHeader/CategoriesList.js
+++ components/SiteHeader/CategoriesList.js
@@ -1,39 +1,18 @@
 import Link from "next/link";
-
-// temp thing - delete later
-const categories = [
-  {
-    id: 1,
-    title: "Mental Health",
-    description: "Learn about tips, mental health issues",
-    link: "/categories/mental-health",
-  },
-  {
-    id: 2,
-    title: "Life Issues",
-    description: "Learn about life issues, product updates and company culture",
-    link: "/categories/life-issues",
-  },
-  {
-    id: 3,
-    title: "Social & Relationships",
-    description:
-      "Learn about social and relationships issues, product updates and company culture",
-    link: "/categories/social-relationship",
-  },
-];
+import useSWR from "swr";
 
 const CategoriesList = () => {
+  const { data: categories } = useSWR("/api/categories");
   return (
     <div>
-      {categories.map((category) => (
-        <Link href={category.link} key={category.id}>
-          <a className="block hover:bg-gray-200 p-6">
-            <h3 className="font-extrabold">{category.title}</h3>
-            <p className="text-gray-500">{category.description}</p>
-          </a>
-        </Link>
-      ))}
+      {categories &&
+        categories.data.map(({ id, slug, name }) => (
+          <Link href={`/categories/${slug}`} key={id}>
+            <a className="block hover:bg-gray-200 p-6">
+              <h3 className="font-extrabold">{name}</h3>
+            </a>
+          </Link>
+        ))}
     </div>
   );
 };
