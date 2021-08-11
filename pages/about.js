ba3dc3da6288f98ed8fb37378a96e42db884f6be
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-08-11 20:18:06 +0100

    admin

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import Layout from "~/components/Layout/Layout";
+import { useContext } from "react";
+import { UserContext } from "./_app";
 
 const AboutPage = () => {
+  const { user } = useContext(UserContext);
   return (
     <Layout title="about">
       <div>
         <h1 className="text-green-800 bg-green-300 font-bold text-5xl p-32">
-          This is About page
+          {`This is About ${user.email}`}
         </h1>
       </div>
     </Layout>
