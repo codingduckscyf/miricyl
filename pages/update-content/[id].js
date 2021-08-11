30ceefc0c6cc097d1f03ea24dbc638bb067f84c8
Oksana Kostenko <kseniavlad31@gmail.com>
2021-08-11 13:58:40 +0100

    added update-content page

---
+++ pages/update-content/[id].js
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Form from "~/components/Form";
+import Layout from "~/components/Layout/Layout";
+
+const UpdateContent = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data: issueContent, error: issueContentError } = useSWR(
+    `/api/content/${id}`
+  ); // fetch content related to an issue
+
+  const handleUpdateContent = async (data) => {
+    const response = await fetch(`/api/content/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      router.push(`/issues/${data.relations}`);
+    } else {
+      setError(true);
+    }
+  };
+
+  if (!issueContent && !issueContentError) {
+    return <div>Loading...</div>;
+  }
+
+  if (!issueContent) {
+    return <div>Not content found.</div>;
+  }
+
+  const data = {
+    title: issueContent.data[0].title,
+    description: issueContent.data[0].description,
+    content_type: issueContent.data[0].content_type,
+    img_url: issueContent.data[0].img_url,
+    video_url: issueContent.data[0].video_url,
+    relations: "",
+  };
+
+  return (
+    <Layout>
+      <div>
+        <Form data={data} submit={handleUpdateContent} />
+      </div>
+    </Layout>
+  );
+};
+
+export default UpdateContent;
