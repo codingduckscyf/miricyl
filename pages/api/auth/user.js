bbce7cec7fe1c6fc3d49e27347ee74062b5e08c1
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-08-12 13:27:08 +0100

    login

---
+++ pages/api/auth/user.js
@@ -0,0 +1,13 @@
+// check for token
+//verify if token exists
+//find the user and send the user back || send invalid
+// check if a user is logged in the frontend
+//useffect check this endpoint
+
+const handler = (req, res) => {};
+
+export default handler;
+// export default (req, res) => {
+// 	console.log(req.cookies);
+// 	res.send('Hello')
+// }
