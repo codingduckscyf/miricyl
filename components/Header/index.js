288a65a1cd54fca2ad47686b3c7f12bf7f86578e
NeginDn <negin.derakhshaan@gmail.com>
2021-08-13 13:15:07 +0100

    header photo position fixed

--- components/Header/index.js
+++ components/Header/index.js
@@ -13,14 +13,14 @@ const Header = ({
     <div
       className={`bg-${backgroundColor}-100 md:h-1/2 max-w-md mx-auto overflow-hidden md:max-w-full`}
     >
-      <div className="md:flex md:justify-around">
+      <div className="md:flex md:justify-between">
         <div className="p-8 lg:p-20 flex flex-col justify-around">
           <h2 className="font-bold tracking-wide text-3xl md:text-5xl">
             {title}
           </h2>
           <p className="mt-2 text-gray-500 text-xl md:text-2xl">{caption}</p>
           {buttonCaption && (
-            <Button caption={buttonCaption} link="/categories" />
+            <Button caption={buttonCaption} link={"#categories"} />
           )}
         </div>
         <div className="md:flex-shrink-0">
