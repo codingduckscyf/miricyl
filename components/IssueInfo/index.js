0b0b35475f3594408ef5b7dd650c034541d07cb4
NeginDn <negin.derakhshaan@gmail.com>
2021-09-05 14:47:53 +0100

    Changed the padding for small screens

--- components/IssueInfo/index.js
+++ components/IssueInfo/index.js
@@ -1,7 +1,7 @@
 const IssueInfo = ({ issueTitle, issueDescription }) => {
   return (
     <div className="container bg-gray-300 w-full text-white p-10 py-16 sm:px-20 mx-auto">
-      <div className="bg-white bg-opacity-80 text-gray-900 p-20 border rounded-xl">
+      <div className="bg-white bg-opacity-80 text-gray-900 md:p-20 p-8 border rounded-xl">
         <h1 className="text-4xl sm:text-6xl font-bold py-4">{issueTitle}</h1>
         <p className="text-l sm:text-xl 1">{issueDescription}</p>
       </div>
